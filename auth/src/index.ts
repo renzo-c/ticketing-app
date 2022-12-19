37014baec2eea7063cba91abd65ef7d7ca281ff6
Renzo Navarro <rcnavarrop@gmail.com>
2022-12-19 13:24:56 -0500

    Define auth endpoints and test express validator

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import { json } from "body-parser";
+import { currentUserRouter } from "./current-user";
+import { signinRouter } from "./signin";
+import { signoutRouter } from "./signout";
+import { signupRouter } from "./signup";
 
 const app = express();
 app.use(json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000! 💻");
