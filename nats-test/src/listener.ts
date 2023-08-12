d4d9e8bcb26f1dfac0565ded7f7354c42a8c3d72
Renzo Navarro <rcnavarrop@gmail.com>
2023-08-12 00:32:32 -0400

    Enable listener to create multiple instances of it, create queue groups

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,20 +1,24 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
-const client = nats.connect("ticketing", "123", {
+const client = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 console.clear();
 client.on("connect", () => {
   console.log("Listener connected to NATS");
-  const subscription = client.subscribe("ticket:created");
+  const subscription = client.subscribe(
+    "ticket:created",
+    // creating queue group to ensure that multiple instances of the same service
+    // are not going to receive the same event
+    "orders-service-queue-group"
+  );
   subscription.on("message", (msg: Message) => {
     const data = msg.getData();
 
     if (typeof data === "string") {
-      console.log(
-        `Received event #${msg.getSequence()}, with data: ${data}`
-      );
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
     }
   });
 });
