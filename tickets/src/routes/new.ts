425ff209025bf5029c1ad71bf85c0d9288fb0b2e
renzo <rcnavarrop@gmail.com>
2024-01-03 16:13:18 -0500

    Create ticket-created-publisher and integrates it to new ticket route

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { requireAuth, validateRequest } from "@rcnp-tickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
+import { TicketCreatedPublisher } from "../events/publishers/ticket-created-publisher";
 
 const router = express.Router();
 
@@ -23,6 +24,13 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+    new TicketCreatedPublisher(client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
+
     res.status(201).send(ticket);
   }
 );
