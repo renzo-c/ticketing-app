aef0deeab08839f3242c64718d4000ba3d56f36e
renzo <rcnavarrop@gmail.com>
2024-01-02 19:44:34 -0500

    Clarifying comments

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,6 +14,7 @@ client.on("connect", () => {
     process.exit();
   });
   // Set up specific options to change the subscription default behaviour
+  // setManualAckMode will let us handle the msg ack manually
   const options = client.subscriptionOptions().setManualAckMode(true);
   const subscription = client.subscribe(
     "ticket:created",
@@ -37,6 +38,7 @@ client.on("connect", () => {
 });
 
 // watcher for interrupt and terminate signals (do not kill this process just yet, let us close our connections)
+// sigint and sigterm occurs each time ts tries to restart our program or any time you hit ctrl-c
 // needed for graceful restart but signal types not supporterd on Windows
 process.on("SIGINT", () => client.close());
 process.on("SIGTERM", () => client.close());
