bbce4e98077968b2a6847e4ac9b96f2ae50b8e16
renzo <rcnavarrop@gmail.com>
2024-01-04 09:44:48 -0500

    Implement ticket update publishing

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,6 +7,8 @@ import {
 } from "@rcnp-tickets/common";
 import { Ticket } from "../models/ticket";
 import { body } from "express-validator";
+import { TicketUpdatedPublisher } from "../events/publishers/ticket-updated-publisher";
+import { natsWrapper } from "../../nats-wrapper";
 
 const router = express.Router();
 
@@ -32,6 +34,12 @@ router.put(
 
     ticket.set({ title: req.body.title, price: req.body.price });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
     res.send(ticket);
   }
 );
