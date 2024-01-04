34cd77e77271d1db00c64cbc330accd16ca08e2e
renzo <rcnavarrop@gmail.com>
2024-01-04 16:57:59 -0500

    Scaffolding order service and creating routing rules

---
+++ orders/src/app.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@rcnp-tickets/common";
+import { Session, SessionData } from "express-session";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
+import { updatedTicketRouter } from "./routes/update";
+
+type JWT = string;
+// Augment express-session with a custom SessionData object
+declare module "express-session" {
+  interface SessionData {
+    jwt: JWT;
+  }
+  interface Session {
+    jwt: JWT;
+  }
+}
+
+declare module "express" {
+  interface Request {
+    session: (Session & JWT) | Partial<SessionData> | null;
+  }
+}
+
+const app = express();
+// Important so express process user's requests whose IP is behind a reversed proxy
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    secure: process.env.NODE_ENV !== "test",
+    signed: false,
+  }) as express.RequestHandler
+);
+
+app.use(currentUser);
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updatedTicketRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/nats-wrapper.ts
@@ -0,0 +1,29 @@
+import nats, { Stan } from "node-nats-streaming";
+
+// Singleton pattern used to implement NATS streaming server
+class NatsWrapper {
+  // Tells typescript that this property might be undefined for a period of time
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      throw new Error("Cannot access NATS client before connecting");
+    }
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+    return new Promise<void>((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("Connected to NATS");
+        resolve();
+      });
+      this._client!.on("error", (err) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
