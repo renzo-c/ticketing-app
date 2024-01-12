16dea27e783c63437f246a5f403c85c6b498a910
renzo <rcnavarrop@gmail.com>
2024-01-12 00:53:16 -0500

    Adding instance of payment-created event in payment new route. Adding payment-created-listener in orders service

---
+++ orders/src/events/listeners/payment-created-listener.ts
@@ -0,0 +1,33 @@
+import {
+  Subjects,
+  Listener,
+  PaymentCreatedEvent,
+  OrderStatus,
+} from "@rcnp-tickets/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Order } from "../../models/order";
+
+export class PaymentCreatedListener extends Listener<PaymentCreatedEvent> {
+  readonly subject = Subjects.PaymentCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: PaymentCreatedEvent["data"], msg: Message) {
+    const order = await Order.findById(data.orderId);
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.set({
+      status: OrderStatus.Complete,
+    });
+    // technically speaking we would have had to emit an order update event
+    // so the order version gets updated in every service. However, we are not
+    // doing that for a matter of simplicity, backed up on the fact that after
+    // an order is complete, it will not be touched again.
+
+    msg.ack();
+  }
+}
--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -11,6 +11,8 @@ import { body } from "express-validator";
 import { Order } from "../models/order";
 import { stripe } from "../stripe";
 import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publishers/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -48,8 +50,14 @@ router.post(
     });
     await payment.save();
 
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
     res.status(201).send({
-      success: true,
+      id: payment.id,
     });
   }
 );
