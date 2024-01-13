15f378f7988c7993cb15419694bb46c720e20c1e
renzo <rcnavarrop@gmail.com>
2024-01-13 10:18:52 -0500

    fixed test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -8,7 +8,7 @@ it("responds with details about the current user", async () => {
     .get("/api/users/currentuser")
     .set("Cookie", cookie) // set different headers on the request
     .send()
-    .expect(400);
+    .expect(200);
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
 
